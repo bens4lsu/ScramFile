f27cb960d1a04e28c123fae6488589d93bbe4b50
Ben Schultz <bens4lsu@gmail.com>
2022-04-06 17:40:06 -0500

    log request.forwarded

--- Sources/App/Controllers/HostController.swift
+++ Sources/App/Controllers/HostController.swift
@@ -14,6 +14,7 @@ class HostController {
     
     func getHostContext(_ req: Request) async throws -> Host {
         let serverHostName = req.application.http.server.configuration.hostname
+        print ("request forward from:" + req.headers.forwarded)
         guard let host = try await Host.query(on: req.db).filter(\.$rootUrl == serverHostName).first() else {
             throw Abort(.internalServerError, reason: "no host configured for \(serverHostName).")
         }
