0f58c894a8aee7ae33388c691069dd6144147ec0
Ben Schultz <bens4lsu@gmail.com>
2022-04-06 17:46:06 -0500

    referring host fix

--- Sources/App/Controllers/HostController.swift
+++ Sources/App/Controllers/HostController.swift
@@ -15,9 +15,13 @@ class HostController {
     func getHostContext(_ req: Request) async throws -> Host {
         let serverHostName = req.application.http.server.configuration.hostname
         print ("request forward from:")
-        print(req.headers.forwarded.first!)
-        guard let host = try await Host.query(on: req.db).filter(\.$rootUrl == serverHostName).first() else {
-            throw Abort(.internalServerError, reason: "no host configured for \(serverHostName).")
+        
+        guard referringHost = req.headers.forwarded.first?.host else {
+            throw Abort(.internalServerError, reason: "Referrig host has no value.")
+        }
+        
+        guard let host = try await Host.query(on: req.db).filter(\.$rootUrl == referringHost).first() else {
+            throw Abort(.internalServerError, reason: "no host configured for \(referringHost).")
         }
         return host
     }
