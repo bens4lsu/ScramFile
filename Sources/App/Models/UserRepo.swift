a3b8f264cc2420322983425453cbe075d83878e2
Ben Schultz <bens4lsu@gmail.com>
2022-10-17 15:42:06 -0500

    minor bug fix in index, working on SecurityController version from the model.

--- Sources/App/Models/UserRepo.swift
+++ Sources/App/Models/UserRepo.swift
@@ -9,6 +9,7 @@ import Vapor
 import Fluent
 import FluentMySQLDriver
 
+
 final class UserRepo: Model, Content, Codable {
     
     static let schema = "tblUserRepos"
@@ -32,12 +33,28 @@ final class UserRepo: Model, Content, Codable {
         self.userId = userId
         self.repoId = repoId
         self.accessLevel = accessLevel
-        
+            
         if id != nil {
             self.$id.exists = true  // 2021.11.19 - need this to fool Fluent into understanding
                                     //              that the id property is set.  It was trying
                                     //              to insert where I needed an update.
         }
+    }
+    
+    func toUserAccessRepoListItem (_ req: Request) -> SecurityController.UserRepoAccess? {
+        guard let id = self.id else {
+            return nil
+        }
+        guard let repo = try await Repo.find(self.repoId, on: req.db) else {
+            return nil
+        }
+        
+        guard let repoName = repo.repoName,
+              let repoFolder = repo.repoFolder
+        else {
+            return nil
+        }
+        let ura = SecurityController.UserRepoAccess (repoId: id, accessLevel: self.accessLevel, repoName: repoName, repoFolder: repoFolder)
     }
 
 }
